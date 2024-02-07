998891f6969c6927b25678f0dda0a65466a608c9
MaksFenek <makstreonin@gmail.com>
2024-02-07 23:10:14 +0300

    apply to grid

--- src/setup/providers/DndBoard.tsx
+++ src/setup/providers/DndBoard.tsx
@@ -1,22 +1,28 @@
-import { DndContext, defaultCoordinates } from "@dnd-kit/core";
+import { DndContext } from "@dnd-kit/core";
 import { createSnapModifier, restrictToWindowEdges } from "@dnd-kit/modifiers";
-import React from "react";
+import React, { useCallback } from "react";
 import { Grid } from "../layout";
 import { Coordinates } from "@dnd-kit/core/dist/types";
-import { ThirstyBar } from "~/widgets/player/ThirstryBar";
-import { UI_BLOCKS } from "../enum/blocks";
 import { useDispatch } from "react-redux";
 import { changeBlockPosition } from "../store";
 
 export const DndBoard = ({ children }: React.PropsWithChildren) => {
   const dispatch = useDispatch();
-  const [{ x, y }, setCoordinates] =
-    React.useState<Coordinates>(defaultCoordinates);
 
   const [gridSize, setGridSize] = React.useState(30);
 
   const snapToGrid = React.useMemo(
     () => createSnapModifier(gridSize),
+    [gridSize]
+  );
+
+  const applyToGrid = useCallback(
+    (delta: Coordinates) => {
+      return {
+        x: Math.ceil(delta.x / gridSize) * gridSize,
+        y: Math.ceil(delta.y / gridSize) * gridSize,
+      };
+    },
     [gridSize]
   );
 
@@ -26,7 +32,10 @@ export const DndBoard = ({ children }: React.PropsWithChildren) => {
         autoScroll={false}
         onDragEnd={({ delta, active }) => {
           dispatch(
-            changeBlockPosition({ id: active.id, x: delta.x, y: delta.y })
+            changeBlockPosition({
+              id: active.id,
+              ...applyToGrid(delta),
+            })
           );
         }}
         modifiers={[snapToGrid, restrictToWindowEdges]}
