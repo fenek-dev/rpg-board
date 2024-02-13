985da7cdddaae2dc1c4f970bc5985682cf3af0bd
MaksFenek <makstreonin@gmail.com>
2024-02-13 22:15:28 +0300

    Add skeleton for future

--- src/widgets/popups/ui/DraggablePopup.tsx
+++ src/widgets/popups/ui/DraggablePopup.tsx
@@ -1,9 +1,10 @@
-import React, { useCallback } from 'react';
+import React, { Suspense, useCallback } from 'react';
 import Draggable, { DraggableEventHandler } from 'react-draggable';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { RootState } from '~/app/store';
 import { Card, CardContent } from '~/shared/components/ui/card';
+import { Skeleton } from '~/shared/components/ui/skeleton';
 import { popupContainerPositionStyle } from '~/shared/utils';
 
 import { changePopupPosition } from '../store/popups.slice';
@@ -34,9 +35,18 @@ export const DraggablePopup = React.memo(({ children, id, popup }: React.PropsWi
   return (
     <Draggable onStop={handleStop} position={popup}>
       <Card className="absolute z-10" id={id}>
-        <Content gridSize={gridSize} height={popup.height} width={popup.width}>
-          {children}
-        </Content>
+        <Suspense
+          fallback={
+            <Skeleton
+              className="rounded-xl border border-input"
+              style={popupContainerPositionStyle(gridSize, popup.width, popup.height)}
+            />
+          }
+        >
+          <Content gridSize={gridSize} height={popup.height} width={popup.width}>
+            {children}
+          </Content>
+        </Suspense>
       </Card>
     </Draggable>
   );
