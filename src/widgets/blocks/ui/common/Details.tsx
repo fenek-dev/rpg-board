fcd775da17c659b3913751e93a22329a95c2c8b5
MaksFenek <makstreonin@gmail.com>
2024-02-16 22:24:34 +0300

    Details opening improvements

--- src/widgets/blocks/ui/common/Details.tsx
+++ src/widgets/blocks/ui/common/Details.tsx
@@ -10,29 +10,52 @@ interface DetailsProps {
 }
 
 export const Details = ({ block, children }: React.PropsWithChildren<DetailsProps>) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const handleMouseEnter = () => {
+    setIsOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsOpen(false);
+  };
+
+  const handleDragStart = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <HoverCard>
-      <HoverCardTrigger asChild>{children}</HoverCardTrigger>
-      <HoverCardContent align="start" className="min-w-40 max-w-80" side="right">
-        <div className="space-y-2">
-          <h4 className="text-lg font-semibold">{block.name}</h4>
-          <p className="text-sm text-muted-foreground">{block.description}</p>
-          <Separator />
-          <div className="mt-4 flex gap-0.5">
-            <Badge className="" variant="outline">
-              {block.category}
-            </Badge>
-            <Badge className="" rarity={block.rarity} variant="outline">
-              {block.rarity}
-            </Badge>
+    <HoverCard open={isOpen}>
+      <HoverCardTrigger
+        asChild
+        onDragStart={handleDragStart}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        {children}
+      </HoverCardTrigger>
+      {isOpen && (
+        <HoverCardContent align="start" className="min-w-40 max-w-80" side="right">
+          <div className="space-y-2">
+            <h4 className="text-lg font-semibold">{block.name}</h4>
+            <p className="text-sm text-muted-foreground">{block.description}</p>
+            <Separator />
+            <div className="mt-4 flex gap-0.5">
+              <Badge className="" variant="outline">
+                {block.category}
+              </Badge>
+              <Badge className="" rarity={block.rarity} variant="outline">
+                {block.rarity}
+              </Badge>
+            </div>
+            <Separator />
+            <div className="flex justify-between p-1 text-xs text-muted-foreground">
+              <span title="Weight">𐄷 {block.weight}g</span>
+              <span title="Cost">© {block.cost}</span>
+            </div>
           </div>
-          <Separator />
-          <div className="flex justify-between p-1 text-xs text-muted-foreground">
-            <span title="Weight">𐄷 {block.weight}g</span>
-            <span title="Cost">© {block.cost}</span>
-          </div>
-        </div>
-      </HoverCardContent>
+        </HoverCardContent>
+      )}
     </HoverCard>
   );
 };
