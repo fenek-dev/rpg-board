2858c90cb5f8c1a1ed5d07e03a26b2098e443a51
MaksFenek <makstreonin@gmail.com>
2024-02-23 15:19:50 +0300

    Fix puBlocksTogether for shop

--- src/app/store/middlewares/shop.ts
+++ src/app/store/middlewares/shop.ts
@@ -2,7 +2,7 @@ import { Middleware, PayloadAction } from '@reduxjs/toolkit';
 import { get } from 'lodash-es';
 
 import BASIC_POPUPS from '~/entities/constant/popup';
-import { changeBlockPosition } from '~/widgets/blocks/store';
+import { changeBlockPosition, putBlocksTogether } from '~/widgets/blocks/store';
 import { gainMoney, spendMoney } from '~/widgets/player/store';
 
 import { RootState } from '../store';
@@ -10,17 +10,29 @@ import { RootState } from '../store';
 export const shopMiddleware: Middleware<unknown, RootState> = (storeApi) => (next) => (ac) => {
   const action = ac as PayloadAction<unknown>;
 
+  const sell = (cost: number) => {
+    next(action);
+    next(gainMoney(cost));
+    return;
+  };
+
+  const buy = (cost: number) => {
+    const money = storeApi.getState().player.money;
+    next(spendMoney(cost));
+    if (cost <= money) {
+      next(action);
+    }
+    return;
+  };
+
   if (action.type === changeBlockPosition.type) {
     const a = action as ReturnType<typeof changeBlockPosition>;
     const block = get(storeApi.getState().blocks.blocks, a.payload.id);
-    const money = storeApi.getState().player.money;
     const cost = block.amount * block.cost;
 
     // SELL
     if (block && a.payload.belong === BASIC_POPUPS.Shop.container_id && a.payload.belong !== block.belong) {
-      next(action);
-      next(gainMoney(cost));
-      return;
+      sell(cost);
     }
     // BUY
     else if (
@@ -28,11 +40,23 @@ export const shopMiddleware: Middleware<unknown, RootState> = (storeApi) => (nex
       block.belong === BASIC_POPUPS.Shop.container_id &&
       a.payload.belong !== BASIC_POPUPS.Shop.container_id
     ) {
-      next(spendMoney(cost));
-      if (cost <= money) {
-        next(action);
-      }
-      return;
+      buy(cost);
+    }
+  }
+  if (action.type === putBlocksTogether.type) {
+    const a = action as ReturnType<typeof putBlocksTogether>;
+
+    const from = get(storeApi.getState().blocks.blocks, a.payload.from);
+    const to = get(storeApi.getState().blocks.blocks, a.payload.to);
+    const cost = from.amount * from.cost;
+
+    // SELL
+    if (from && to && to.belong === BASIC_POPUPS.Shop.container_id && to.belong !== from.belong) {
+      sell(cost);
+    }
+    // BUY
+    else if (from && from.belong === BASIC_POPUPS.Shop.container_id && to.belong !== BASIC_POPUPS.Shop.container_id) {
+      buy(cost);
     }
   }
   next(action);
