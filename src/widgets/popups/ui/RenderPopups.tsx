768ae7c5c45744e2772704394785466bd5f47442
MaksFenek <makstreonin@gmail.com>
2024-03-03 17:24:30 +0300

    Change coins from inventory to header

--- src/widgets/popups/ui/RenderPopups.tsx
+++ src/widgets/popups/ui/RenderPopups.tsx
@@ -6,7 +6,6 @@ import { EquipmentPopup } from '~/widgets/equipment/ui/EquipmentPopup';
 import { Status } from '~/widgets/player/ui/Status';
 
 import { PopupWithGrid } from './containers/PopupWithGrid';
-import { Inventory } from './special/Inventory';
 
 export const RenderPopups = () => {
   const popups = useSelector((state: RootState) => state.popups.popups);
@@ -16,10 +15,6 @@ export const RenderPopups = () => {
       {Object.keys(popups).map((id) => {
         if (id === BASIC_POPUPS.Status.container_id) {
           return <Status key={id} />;
-        }
-
-        if (id === BASIC_POPUPS.Inventory.container_id) {
-          return <Inventory id={id} key={id} />;
         }
 
         if (id === BASIC_POPUPS.Equipment.container_id) {
--- src/widgets/settings/ui/Header.tsx
+++ src/widgets/settings/ui/Header.tsx
@@ -1,9 +1,17 @@
+import { useSelector } from 'react-redux';
+
+import { RootState } from '~/app/store';
+import { Button } from '~/shared/components/ui/button';
+
 import { Menu } from './Menu';
 
 export const Header = () => {
+  const coins = useSelector((state: RootState) => state.player.money);
   return (
-    <div className="fixed left-0 right-0 top-0 z-50 flex justify-between border-b border-input bg-background p-2 px-6">
-      <div></div>
+    <div className="fixed left-0 right-0 top-0 z-50 flex items-center justify-between border-b border-input bg-background p-2 px-6">
+      <div>
+        <Button variant="outline">{coins} 🪙</Button>
+      </div>
       <div>
         <Menu />
       </div>
