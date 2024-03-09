2fcae592361a15a5f562ba51401e9c7c863cc1a5
MaksFenek <makstreonin@gmail.com>
2024-03-09 19:40:42 +0300

    Fix lint

--- src/widgets/combat/utils/loot.ts
+++ src/widgets/combat/utils/loot.ts
@@ -4,31 +4,42 @@ import { Item } from '~/entities/extendable/items';
 import { Loot } from '~/entities/extendable/loot';
 import { Stages } from '~/entities/extendable/map';
 import { ITEMS } from '~/entities/items';
-import { STAGES, getLootByType } from '~/entities/stage/stages';
+import { getLootByType } from '~/entities/stage/stages';
 import { mulberry32 } from '~/shared/utils/random';
 import { Block } from '~/widgets/blocks/store';
 
 import { CombatTypes } from '../store/combat.types';
 
-export const generateLoot = (stage: Stages, belongs: string, type: CombatTypes, seed?: number): Block[] => {
+export const generateLoot = (
+  stage: Stages,
+  belongs: string,
+  type: CombatTypes,
+  seed?: number
+): Record<string, Block> => {
   const prng = mulberry32(seed || random(0, Math.pow(2, 32)));
   const loot: Loot[] = getLootByType(stage, type);
 
-  return loot
-    .filter((l) => prng() * 100 <= l.chance)
+  return Object.fromEntries(
+    loot
+      .filter((l) => prng() * 100 <= l.chance)
 
-    .map((l) => {
-      const amount = Array.isArray(l.amount)
-        ? Math.floor(prng() * (l.amount[1] - l.amount[0]) + l.amount[0])
-        : l.amount;
-      return {
-        ...ITEMS[l.id],
-        amount,
-        belong: belongs,
-        exp: 0,
-        id: l.id,
-        x: 0,
-        y: 0,
-      } as Block<Item>;
-    });
+      .map((l) => {
+        const amount = Array.isArray(l.amount)
+          ? Math.floor(prng() * (l.amount[1] - l.amount[0]) + l.amount[0])
+          : l.amount;
+        const id = crypto.randomUUID();
+        return [
+          id,
+          {
+            ...ITEMS[l.id],
+            amount,
+            belong: belongs,
+            exp: 0,
+            id: l.id,
+            x: 0,
+            y: 0,
+          } as Block<Item>,
+        ];
+      })
+  );
 };
