09399ec273b28f1ee83b25e152513f50b9d5b6f6
MaksFenek <makstreonin@gmail.com>
2024-03-09 12:13:09 +0300

    Entity hp bar delta

--- src/shared/components/ui/progress.tsx
+++ src/shared/components/ui/progress.tsx
@@ -27,25 +27,29 @@ const Progress = React.forwardRef<
 ));
 Progress.displayName = ProgressPrimitive.Root.displayName;
 
+interface ProgressDisplayProps extends ProgressProps {
+  progressDelta?: number;
+}
+
 const ProgressDisplay = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & ProgressProps
->(({ className, indicatorClassName, max, value, ...props }, ref) => (
-  <ProgressPrimitive.Root
-    className={cn('relative h-2 w-full overflow-hidden rounded-full bg-primary/20', className)}
-    max={max}
-    ref={ref}
-    value={value}
-    {...props}
-  >
-    <span className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 text-sm leading-none text-muted-foreground">
-      {value} / {max}
-    </span>
-    <ProgressPrimitive.Indicator
-      className={cn('h-full w-full flex-1 bg-primary transition-all', indicatorClassName)}
-      style={{ transform: `translateX(-${100 - ((value! / max!) * 100 || 0)}%)` }}
-    />
-  </ProgressPrimitive.Root>
-));
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & ProgressDisplayProps
+>(({ children, className, indicatorClassName, max, progressDelta, value, ...props }, ref) => {
+  return (
+    <ProgressPrimitive.Root
+      className={cn('relative h-2 w-full overflow-hidden rounded-full bg-primary/20', className)}
+      max={max}
+      ref={ref}
+      value={value}
+      {...props}
+    >
+      {children}
+      <ProgressPrimitive.Indicator
+        className={cn('z-10 h-full w-full flex-1 bg-primary transition-all', indicatorClassName)}
+        style={{ transform: `translateX(-${100 - (((value! - (progressDelta || 0)) / max!) * 100 || 0)}%)` }}
+      />
+    </ProgressPrimitive.Root>
+  );
+});
 
 export { Progress, ProgressDisplay };
--- src/widgets/combat/ui/Entity/EntityBar.tsx
+++ src/widgets/combat/ui/Entity/EntityBar.tsx
@@ -1,4 +1,4 @@
-import { Progress } from '~/shared/components/ui/progress';
+import { ProgressDisplay } from '~/shared/components/ui/progress';
 
 interface EntityBarProps {
   color: string;
@@ -10,7 +10,7 @@ interface EntityBarProps {
 export const EntityBar = ({ color, dmg, max, value }: EntityBarProps) => {
   return (
     <span className="relative mt-2 w-full">
-      <Progress indicatorClassName={color} max={max} value={value} />
+      <ProgressDisplay indicatorClassName={color} max={max} progressDelta={dmg} value={value} />
       <span className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 whitespace-nowrap text-base font-bold leading-none">
         {value}
         {dmg && <span className="text-sm text-teal-600">(-{dmg})</span>} / {max}
